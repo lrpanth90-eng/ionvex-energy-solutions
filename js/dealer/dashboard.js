117584d18c0eebd5ea9e0b0efb8fa19099c3efb3
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 12:50:09 +0530

    Update dashboard.js

--- js/dealer/dashboard.js
+++ js/dealer/dashboard.js
@@ -1,33 +1,9 @@
-import { auth, db } from "../firebase/app.js";
-import { signOut, onAuthStateChanged } from
+import { auth } from "../firebase/app.js";
+import { onAuthStateChanged } from
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import {
-  collection,
-  query,
-  where,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const totalCount = document.getElementById("totalCount");
-const logoutBtn = document.getElementById("logoutBtn");
-
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
   if (!user) {
-    window.location.href = "/login.html";
-    return;
+    window.location.replace("/login.html");
   }
-
-  const q = query(
-    collection(db, "batteries"),
-    where("activatedBy", "==", user.uid)
-  );
-
-  const snap = await getDocs(q);
-  totalCount.innerText = snap.size;
 });
-
-logoutBtn.onclick = async () => {
-  await signOut(auth);
-  window.location.href = "/login.html";
-};
