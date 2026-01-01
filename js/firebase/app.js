e52fc6b519709a95b6fcdfd7d606e468de403e4e
lrpanth90-eng <lrpanth90@gmail.com>
2026-01-01 08:26:17 +0530

    Update app.js

--- js/firebase/app.js
+++ js/firebase/app.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDvV77_17S233OHTDxxOHaEIeruo_IP-u8",
+  authDomain: "ionvex-energy.firebaseapp.com",
+  projectId: "ionvex-energy",
+  storageBucket: "ionvex-energy.firebasestorage.app",
+  messagingSenderId: "48652442204",
+  appId: "1:48652442204:web:f23a353804282630c1e499"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
